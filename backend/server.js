4d935986962aacd773b99e32ff2d59e7b0064423
llumiine <laamahmed06@gmail.com>
2025-10-07 10:19:13 +0200

    modif frontend

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,32 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors'); 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
-app.use(cors()); 
+app.use(cors());
 
 app.use(express.json());
 
-const routeurContact = require('./controller/routeurContact.js');
-const routeurAuth = require('./controller/authController.js');
-const { swaggerUi, swaggerSpec } = require('./swagger'); 
+const routeurContact = require("./controller/routeurContact.js");
+const routeurAuth = require("./controller/authController.js");
+const { swaggerUi, swaggerSpec } = require("./swagger");
 
+app.use("/api/contacts", routeurContact);
+app.use("/api/auth", routeurAuth);
 
-app.use('/api/contacts', routeurContact);
-app.use('/api/auth', routeurAuth);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-mongoose.connect(
-  "mongodb+srv://laamahmed:8nBn8jKIAUG25HfC@mycontact.gmiwqrx.mongodb.net/Node-API?retryWrites=true&w=majority&appName=MyContact"
-).then(() => {
-  console.log("BD Connectée");
-}).catch((err) => {
-  console.error("Erreur de connexion à MongoDB :", err.message);
-});
+mongoose
+  .connect(
+    "mongodb+srv://laamahmed:8nBn8jKIAUG25HfC@mycontact.gmiwqrx.mongodb.net/Node-API?retryWrites=true&w=majority&appName=MyContact"
+  )
+  .then(() => {
+    console.log("BD Connectée");
+  })
+  .catch((err) => {
+    console.error("Erreur de connexion à MongoDB :", err.message);
+  });
 
 app.listen(8080, () => {
-  console.log('Server is running on http://localhost:8080');
+  console.log("Server is running on http://localhost:8080");
 });
-
-
-
-
-
-
-
-
--- frontend/src/Accueil.js
+++ frontend/src/Accueil.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import mimi from './mimi.jpg';
 
 export default function Accueil() {
   return (
     <div>
-      <h1>Accueil</h1>
-            <Link to="/inscription">
-              <button>Inscription</button>
-            </Link>
-      <Link to="/connexion">
-        <button>Connexion</button>
-      </Link>
+      <h1>Accueil: Veuillez vous connecter ou vous inscrire</h1>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img src={mimi} alt="mimi" style={{ maxWidth: '200px', marginRight: '20px' }} />
+        <div>
+          <Link to="/inscription">
+            <button>Inscription</button>
+          </Link>
+          <br />
+          <Link to="/connexion">
+            <button>Connexion</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
